ddae54ae5b1b7cc4b40072e29c56a6498370232d
Kaio Silveira <silveira.kaio@icloud.com>
2024-03-30 10:27:01 +0000

    rename internal `_name` to `_title`

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 export class Organization {
   constructor(data) {
-    this._name = data.name;
+    this._title = data.name;
     this._country = data.country;
   }
 
   get name() {
-    return this._name;
+    return this._title;
   }
 
   set name(aString) {
-    this._name = aString;
+    this._title = aString;
   }
 
   get country() {
