dcfb77e2ffbda63055a7fc8a6a14095320c3b861
Kaio Silveira <silveira.kaio@icloud.com>
2024-03-30 10:31:21 +0000

    rename `name` getter to `title`

--- src/index.test.js
+++ src/index.test.js
@@ -2,12 +2,12 @@ import { Organization, organization } from './index';
 
 describe('Organization', () => {
   it('should have a name and a country', () => {
-    expect(organization.name).toBe('Acme Gooseberries');
+    expect(organization.title).toBe('Acme Gooseberries');
     expect(organization.country).toBe('GB');
   });
 
   it('should support a title', () => {
     const org = new Organization({ title: 'Acme Gooseberries', country: 'GB' });
-    expect(org.name).toBe('Acme Gooseberries');
+    expect(org.title).toBe('Acme Gooseberries');
   });
 });
