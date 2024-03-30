45c572cd4479f30630d4f966cb15a9cecca8cb94
Kaio Silveira <silveira.kaio@icloud.com>
2024-03-30 10:25:24 +0000

    encapsulate `organization` in a class

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-export const organization = { name: 'Acme Gooseberries', country: 'GB' };
+export class Organization {
+  constructor(data) {
+    this._name = data.name;
+    this._country = data.country;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(aString) {
+    this._name = aString;
+  }
+
+  get country() {
+    return this._country;
+  }
+
+  set country(aCountryCode) {
+    this._country = aCountryCode;
+  }
+}
+
+export const organization = new Organization({ name: 'Acme Gooseberries', country: 'GB' });
